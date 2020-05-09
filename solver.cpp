9e93405cad8b83fd40a6490cb7161b0543b86f5a
shaykeshok <shayke@EPR-248-Shayke.es.local>
2020-05-09 22:16:47 +0300

    complex

--- solver.cpp
+++ solver.cpp
@@ -42,6 +42,45 @@ double solve(RealVariable &r1)
     }
     return ans;
 };
+std::complex<double> solve(ComplexVariable &c1)
+{
+    //c1.toString("solve");
+    double a = c1.coef_2;
+    double b = c1.coef_1;
+    std::complex<double> c = c1.comp;
+    std::complex<double> ans;
+
+    if (a != 0) // if coef_2 of y^2 is 0 the division by 0 in the roots furmula is invalid
+    {
+
+        ans = pow(b, 2) - 4 * a * c; //b^2-4ac
+        if (c.imag() != 0)
+        {
+            throw runtime_error("Can't perform sqrt to imaginary variable!");
+        }
+        ans = sqrt(ans); //sqrt(b^2-4ac)
+        ans = (ans - b) / (2 * a);
+    }
+    else if (b != 0)
+    {
+        ans = (c * -1.0) / b;
+    }
+    else
+    {
+        throw runtime_error("Error!");
+    }
+    if (ans.imag() == 0)
+    {
+        ans.imag(0);
+    }
+    if (ans.real() == 0)
+    {
+        ans.real(0);
+    }
+    return ans;
+};
+
+//----------------------------------------------------------RealVariable--------------------------------------------------//
 
 void RealVariable::toString()
 {
@@ -49,13 +88,13 @@ void RealVariable::toString()
     cout << "b: " << this->coef_1 << endl;
     cout << "c: " << this->coef_0 << endl;
 }
-
 void RealVariable::copy(RealVariable &real1)
 {
     this->coef_2 = real1.coef_2;
     this->coef_1 = real1.coef_1;
     this->coef_0 = real1.coef_0;
 }
+
 RealVariable &operator-(RealVariable &real1, double x)
 {
     double c = real1.coef_0;
@@ -65,6 +104,7 @@ RealVariable &operator-(RealVariable &real1, double x)
     temp->coef_0 -= x;
     return *temp;
 };
+
 RealVariable &operator-(RealVariable &real1, RealVariable &real2)
 {
     double c = real1.coef_0;
@@ -183,6 +223,157 @@ RealVariable &operator+(RealVariable &real1, RealVariable &real2)
     temp->coef_1 += real2.coef_1;
     temp->coef_2 += real2.coef_2;
     return *temp;
+}
+
+//----------------------------------------------------------ComplexVariable--------------------------------------------------//
+ComplexVariable &operator*(double x, ComplexVariable &complex)
+{
+    //complex.toString("operator*");
+    double b = complex.coef_1;
+    double a = complex.coef_2;
+    std::complex<double> c = complex.comp;
+
+    ComplexVariable *temp = new ComplexVariable(a, b);
+    temp->coef_1 *= x;
+    temp->coef_2 *= x;
+    temp->comp *= x;
+    //temp->toString("operator*    After:");
+
+    return *temp;
+}
+ComplexVariable &operator*(ComplexVariable &complex, double x)
+{
+
+    //complex.toString("operator*");
+    return x * complex;
+}
+
+ComplexVariable &operator/(ComplexVariable &complex, double num)
+{
+    double b = complex.coef_1;
+    double a = complex.coef_2;
+    std::complex<double> c = complex.comp;
+    ComplexVariable *temp = new ComplexVariable(a, b, c);
+
+    temp->coef_2 /= num;
+    temp->coef_1 /= num;
+    temp->comp /= num;
+
+    return *temp;
+}
+
+ComplexVariable &operator-(ComplexVariable &comp, double x)
+{
+    // double b = comp.coef_1;
+    // double a = comp.coef_2;
+    // ComplexVariable *temp = new ComplexVariable(a, b);
+    // temp->coef_0 -= x;
+    // return *temp;
+    //comp.toString("operator-");
+    return comp + (-1) * x;
+}
+
+ComplexVariable &operator-(ComplexVariable &x, ComplexVariable &y)
+{
+    double b = x.coef_1;
+    double a = x.coef_2;
+    std::complex<double> c = x.comp;
+    ComplexVariable *temp = new ComplexVariable(a, b, c);
+    temp->coef_1 -= y.coef_1;
+    temp->coef_2 -= y.coef_2;
+    temp->comp -= y.comp;
+    return *temp;
+}
+ComplexVariable &operator+(ComplexVariable &complex, double y)
+{
+    //complex.toString("operator+");
+
+    double b = complex.coef_1;
+    double a = complex.coef_2;
+    std::complex<double> c = complex.comp;
+    ComplexVariable *temp = new ComplexVariable(a, b, c);
+    temp->comp += y;
+    //temp->toString("operator+     After:");
+    return *temp;
+}
+ComplexVariable &operator+(double y, ComplexVariable &complex)
+{
+    //complex.toString("operator+");
+    return complex + y;
+}
+ComplexVariable &operator+(ComplexVariable &x, ComplexVariable &y)
+{
+    //x.toString("operator+");
+    //y.toString("operator+");
+
+    double b = x.coef_1;
+    double a = x.coef_2;
+    std::complex<double> c = x.comp;
+    ComplexVariable *temp = new ComplexVariable(a, b, c);
+    temp->coef_1 += y.coef_1;
+    temp->coef_2 += y.coef_2;
+    temp->comp += y.comp;
+    //temp->toString("operator+     After:");
+    return *temp;
+}
+ComplexVariable &operator+(ComplexVariable &x, complex<double> comp)
+{
+    double b = x.coef_1;
+    double a = x.coef_2;
+    std::complex<double> c = x.comp;
+    ComplexVariable *temp = new ComplexVariable(a, b, c);
+    temp->comp += comp;
+    return *temp;
+}
+ComplexVariable &operator==(ComplexVariable &comp, double x)
+{
+    //comp.toString("operator==");
+
+    return comp + (-1) * x;
+}
+ComplexVariable &operator==(ComplexVariable &x, ComplexVariable &y)
+{
+    double b = x.coef_1;
+    double a = x.coef_2;
+    std::complex<double> c = x.comp;
+    ComplexVariable *temp = new ComplexVariable(a, b, c);
+    temp->coef_1 -= y.coef_1;
+    temp->coef_2 -= y.coef_2;
+    temp->comp -= y.comp;
+    return *temp;
+}
+ComplexVariable &operator^(ComplexVariable &complex, double x)
+{
+    if (x > 2)
+    {
+        throw runtime_error("ERROR: Not Second-degree equation");
+    }
+
+    double b = complex.coef_1;
+    double a = complex.coef_2;
+    std::complex<double> c = complex.comp;
+    ComplexVariable *temp = new ComplexVariable(a, b, c);
+
+    if (temp->coef_2 != 0)
+    {
+        throw runtime_error("ERROR: Not Second-degree equation");
+    }
+    else
+    {
+        temp->coef_2 = pow(b, 2);
+        temp->coef_1 = 0;
+        temp->comp *= temp->comp;
+    }
+
+    return *temp;
+}
+void ComplexVariable::toString(string msg)
+{
+    cout << "\n"
+         << msg << endl;
+    cout << "a: " << this->coef_2 << endl;
+    cout << "b: " << this->coef_1 << endl;
+    cout << "c: " << this->comp << endl;
 }
 
 }; // namespace solver
--- solver.hpp
+++ solver.hpp
@@ -14,11 +14,9 @@ public:
     double coef_1; //x^1
     double coef_0; //x^0
 
-
-
     //constructors:
-    RealVariable(): coef_2(0), coef_1(1), coef_0(0) {}; //default constructor                          
-    RealVariable(double c2, double c1, double c0): coef_2(c2), coef_1(c1), coef_0(c0) {}
+    RealVariable() : coef_2(0), coef_1(1), coef_0(0){}; //default constructor
+    RealVariable(double c2, double c1, double c0) : coef_2(c2), coef_1(c1), coef_0(c0) {}
     //overloads operators:
 
     //operator '*'
@@ -28,7 +26,7 @@ public:
     friend RealVariable &operator==(RealVariable &real, double x);
     friend RealVariable &operator==(RealVariable &real1, RealVariable &real2);
 
-        //operator '^'
+    //operator '^'
     friend RealVariable &operator^(double x, RealVariable &real);
     friend RealVariable &operator^(RealVariable &real1, double x);
 
@@ -36,9 +34,8 @@ public:
     friend RealVariable &operator-(RealVariable &real, double x);
     friend RealVariable &operator-(RealVariable &real1, RealVariable &real2);
 
-
     //operator '+'
-    friend RealVariable &operator+(RealVariable &real1,double x);
+    friend RealVariable &operator+(RealVariable &real1, double x);
     friend RealVariable &operator+(double x, RealVariable &real);
     friend RealVariable &operator+(RealVariable &real1, RealVariable &real2);
 
@@ -48,5 +45,55 @@ public:
     void copy(RealVariable &r1);
 };
 double solve(RealVariable &r1);
+
+class ComplexVariable
+{
+
+public:
+    // complex<double> num;
+    //a+bi
+    double coef_1; //y^1
+    double coef_2; //y^2
+    complex<double> comp = 0. + 0i;
+
+    ComplexVariable() : coef_2(0), coef_1(1){}; //default constructor
+    ComplexVariable(double c2, double c1) : coef_2(c2), coef_1(c1) {}
+    ComplexVariable(double c2, double c1, complex<double> c0) : coef_2(c2), coef_1(c1), comp(c0) {}
+
+    //overloads operators:
+
+    //operator '*'
+    friend ComplexVariable &operator*(double num, ComplexVariable &x);
+
+    //operator '=='
+    friend ComplexVariable &operator==(ComplexVariable &x, double num);
+    // friend ComplexVariable &operator==(double num, ComplexVariable &x);
+    friend ComplexVariable &operator==(ComplexVariable &x, ComplexVariable &y);
+    // friend ComplexVariable &operator==(ComplexVariable &x, complex<double> comp);
+    // friend ComplexVariable &operator==(complex<double> comp, ComplexVariable &x);
+
+    //operator '^'
+    friend ComplexVariable &operator^(ComplexVariable &x, double num);
+
+    //operator '-'
+    friend ComplexVariable &operator-(ComplexVariable &x, double num);
+    // friend ComplexVariable &operator-(double num, ComplexVariable &x);
+    friend ComplexVariable &operator-(ComplexVariable &x, ComplexVariable &y);
+    // friend ComplexVariable &operator-(complex<double> comp, ComplexVariable &x);
+    // friend ComplexVariable &operator-(ComplexVariable &x, complex<double> comp);
+
+    //operator '+'
+    friend ComplexVariable &operator+(ComplexVariable &x, double num);
+    friend ComplexVariable &operator+(double num, ComplexVariable &x);
+    friend ComplexVariable &operator+(ComplexVariable &x, ComplexVariable &y);
+    friend ComplexVariable &operator+(ComplexVariable &x, complex<double> comp);
+    // friend ComplexVariable &operator+(complex<double> comp, ComplexVariable &x);
+
+    //operator '/'
+    friend ComplexVariable &operator/(ComplexVariable &x, double num);
+    void toString(string msg);
+};
+
+std::complex<double> solve(ComplexVariable &c1);
 
 }; // namespace solver
